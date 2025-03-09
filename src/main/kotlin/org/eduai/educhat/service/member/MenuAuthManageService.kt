9b40ec9362d91eff4b6c253e27c8ccd15356baf7
hjbyeon <hjbyeon@wise.co.kr>
2025-03-09 22:12:20 +0900

    2025/03/09 최초 메뉴 권한 조회시 모든 메뉴 허용하도록 DB에 저장하는 로직 추가

--- src/main/kotlin/org/eduai/educhat/service/member/MenuAuthManageService.kt
+++ src/main/kotlin/org/eduai/educhat/service/member/MenuAuthManageService.kt
@@ -2,6 +2,7 @@ package org.eduai.educhat.service.member
 
 import org.eduai.educhat.dto.member.request.SetMenuAuthRequestDto
 import org.eduai.educhat.dto.member.response.SetMenuAuthResponseDto
+import org.eduai.educhat.entity.ClsMenuAuth
 import org.eduai.educhat.repository.ClsMenuAuthRepository
 import org.springframework.stereotype.Service
 
@@ -19,10 +20,23 @@ class MenuAuthManageService(
     }
 
     fun getMenuAuth(clsId: String): SetMenuAuthResponseDto {
-        val menuAuth = clsMenuAuthRepository.findById(clsId)
-            .orElse(null) ?: return SetMenuAuthResponseDto()  // 값이 없으면 빈 DTO 반환
-
-        return SetMenuAuthResponseDto(menuAuth.componentList?.split(","))
+        val menuAuthOpt = clsMenuAuthRepository.findById(clsId)
+        return if (menuAuthOpt.isPresent) {
+            val menuAuth = menuAuthOpt.get()
+            SetMenuAuthResponseDto(menuAuth.componentList?.split(","))
+        } else {
+            // 기본 허용 메뉴 리스트
+            val defaultMenus = listOf("/review", "/preview", "/schat", "/topicextraction", "/sdiscussion")
+            val defaultComponentList = defaultMenus.joinToString(",")
+            // 새로운 ClsMenuAuth 객체 생성 (필요한 필드에 맞게 수정)
+            val newMenuAuth = ClsMenuAuth(
+                clsId = clsId,
+                componentList = defaultComponentList
+            )
+            // DB에 저장
+            clsMenuAuthRepository.save(newMenuAuth)
+            SetMenuAuthResponseDto(defaultMenus)
+        }
     }
 
 
